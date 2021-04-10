790a6c0a49f0370042e2c8d71a326452e48bd64d
Pingl20 <79899248+Pingl20@users.noreply.github.com>
2021-04-10 13:40:14 +0800

    add more descriptive pseudocode

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,10 +1,12 @@
-#define the number and the rate
-
+#define the number
 n = 84
+#define the rate
 r =1.2
+#define a variable used to count
 i = 0
 while i < 5:
     n = n + n*r
     i += 1
-print("after 5 rounds, when r rate = %.2f, the number "
+#print what we need in a clear way
+    print("after 5 rounds, when r rate = %.2f, the number "
       "of infected will be %d" % (r, n))
