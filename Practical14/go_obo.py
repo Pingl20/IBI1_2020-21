cdb86bc96555f880c25d4d26644eb11031dccf2e
Ping Lu <pingl.20@intl.zju.edu.cn>
2021-05-17 15:25:27 +0800

    Update go_obo.py

--- Practical14/go_obo.py
+++ Practical14/go_obo.py
@@ -1,5 +1,6 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
+import matplotlib.pyplot as plt
 
 DOMTree = xml.dom.minidom.parse("D:\\Github\\IBI1_2020-21\\Practical14\\go_obo.xml")
 collection = DOMTree.documentElement
@@ -43,25 +44,30 @@ def allChildren(go, name_go):
 
 def printResult(go, name_go, name, bug_name):
     name_count = len(list(set(allChildren(go, name_go)))) - len(name_go) - len(bug_name)
-    print("The number of childNodes for each {}-related gene is {}".format(name, name_count))
-
-
+    return name_count
 # DNA
 dna, bug_dna = goName(terms, "DNA")
-printResult(go, dna, "DNA", bug_dna)
+print("The number of childNodes for each DNA-related gene is {}".format(printResult(go, dna, "DNA", bug_dna)))
     # Output: The number of childNodes for each DNA-related gene is 7271
 
 # RNA
 rna, bug_rna = goName(terms, "RNA")
-printResult(go, rna, "RNA", bug_rna)
+print("The number of childNodes for each RNA-related gene is {}".format(printResult(go, rna, "RNA", bug_rna)))
     # Output: The number of childNodes for each RNA-related gene is 8927
 
 # protein
 protein, bug_protein = goName(terms, "protein")
-printResult(go, protein, "protein", bug_protein)
+print("The number of childNodes for each protein-related gene is {}".format(printResult(go, protein, "protein", bug_protein)))
     # Output: The number of childNodes for each protein-related gene is 27771
 
 # glycoprotein
 glycoprotein, bug_glycoprotein = goName(terms, "glycoprotein")
-printResult(go, glycoprotein, "glycoprotein", bug_glycoprotein)
+print("The number of childNodes for each glycoprotein-related gene is {}".format(printResult(go, glycoprotein, "glycoprotein", bug_glycoprotein)))
     # Output: The number of childNodes for each glycoprotein-related gene is 150
+
+labels = 'DNA', 'RNA', 'protein', 'glycoprotein'
+values = [printResult(go, dna, "DNA", bug_dna), printResult(go, rna, "RNA", bug_rna),
+          printResult(go, protein, "protein", bug_protein), printResult(go, glycoprotein, "glycoprotein", bug_glycoprotein)]
+plt.pie(values, labels = labels, autopct='%1.1f%%',shadow=False)
+plt.title("Child Nodes of 4 macromolecules")
+plt.show()
